eec6c723e5a229d9b415e3d2aa7954903c0d3d99
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2018-09-27 16:51:40 +0200

    code telt voor meerdere blocken, nu nog automatisch aanvullen via javascript bij herkennen hoeveel blocks in html

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 
 window.onload = function(){
-    setPictures();
+    setPictureBlocks();
 };
 
-function setPictures() {
-    setPicturesInHtml("showPictures", 12, "show");
-    setPicturesInHtml("displayPictures", 3, "display");
-    setSizesPicturesOfClass("show");
-    setSizesPicturesOfClass("display");
+
+function setPictureBlocks() {
+    setPictures("block1");
+    setPictures("block2");
+    setPictures("block3");
 }
-function setPicturesInHtml(divID, amount, className) {
-    var innerHtml = "";
-    for (var i = 1; i < amount + 1; i++){
-        innerHtml += '<img src="pictures/' + i + '.png" class="' + className + '">';
+function setPictures(divID) {
+    setPicturesInHtml(divID);
+
+
+    setSizesPicturesOfClass("ShowPicture", 12);
+    setSizesPicturesOfClass("DisplayPicture", 3);
+}
+function setPicturesInHtml(divID) {
+    var innerHtml = "<div class='Show' id='" + divID + "Show'>";
+    for (var i = 1; i < 13; i++){
+        innerHtml += '<img src="pictures/' + i + '.png" class="ShowPicture" id="' + divID + "ShowPicture" + i + '">';
     }
+    innerHtml += "</div><div class='Display' id='" + divID + "Display'>";
+    for (var i = 1; i < 4; i++){
+        innerHtml += '<img src="pictures/' + i + '.png" class="DisplayPicture" id="' + divID + "DisplayPicture" + i + '">';
+    }
+    innerHtml += "</div>";
     document.getElementById(divID).innerHTML = innerHtml;
 }
-function setSizesPicturesOfClass(className) {
-    var showPictureElements = document.getElementsByClassName(className);
-    for (var i = 0; i < showPictureElements.length; i++){
-        showPictureElements[i].style.width = (screen.width/showPictureElements.length) + "px";
-        showPictureElements[i].style.height = (screen.width/showPictureElements.length) * (2/3) + "px";
+
+function setSizesPicturesOfClass(className, amountOfImages) {
+    var pictureElements = document.getElementsByClassName(className);
+    var length = pictureElements.length;
+    for (var i = 0; i < length; i++){
+        pictureElements[i].style.width = (screen.width/(length / (length / amountOfImages))) + "px";
+        pictureElements[i].style.height = (screen.width/(length / (length / amountOfImages))) * (2/3) + "px";
     }
 }
+
