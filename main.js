bdfc83acf54b3662041e78d18bc8c244f0b8ba0e
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2018-09-27 15:11:46 +0200

    displaypictures toegevoegd, verder refactor

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 window.onload = function(){
     setShowPictures();
+    setDisplayPictures();
 };
 
 
@@ -11,12 +12,32 @@ function setShowPictures() {
 function setPicturesInHtml() {
     var innerHtml = "";
     for (var i = 1; i < 13; i++){
-        innerHtml += '<img src=\"pictures/' + i + '.png\" class=\"show\">';
+        innerHtml += '<img src="pictures/' + i + '.png" class="show">';
     }
     document.getElementById("showPictures").innerHTML = innerHtml;
 }
 function setSizesShowPictures() {
     var showPictureElements = document.getElementsByClassName("show");
+    for (var i = 0; i < showPictureElements.length; i++){
+        showPictureElements[i].style.width = (screen.width/showPictureElements.length) + "px";
+        showPictureElements[i].style.height = (screen.width/showPictureElements.length) * (2/3) + "px";
+    }
+}
+
+
+function setDisplayPictures() {
+    setDisplayPicturesInHtml();
+    setSizesDisplayPictures();
+}
+function setDisplayPicturesInHtml() {
+    var innerHtml = "";
+    for (var i = 1; i < 4; i++){
+        innerHtml += '<img src="pictures/' + i + '.png" class="displayPicture">';
+    }
+    document.getElementById("displayPictures").innerHTML = innerHtml;
+}
+function setSizesDisplayPictures() {
+    var showPictureElements = document.getElementsByClassName("displayPicture");
     for (var i = 0; i < showPictureElements.length; i++){
         showPictureElements[i].style.width = (screen.width/showPictureElements.length) + "px";
         showPictureElements[i].style.height = (screen.width/showPictureElements.length) * (2/3) + "px";
