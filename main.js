8219817cdebb1206e97172906f63ea88d717dde8
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2018-09-27 14:53:53 +0200

    showPictures worden gelijnd

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 
 window.onload = function(){
-    document.getElementById("test").innerText = "test";
+    setShowPictures();
 };
+
+
+function setShowPictures() {
+    setPicturesInHtml();
+    setSizesShowPictures();
+}
+function setPicturesInHtml() {
+    var innerHtml = "";
+    for (var i = 1; i < 13; i++){
+        innerHtml += '<img src=\"pictures/' + i + '.png\" class=\"show\">';
+    }
+    document.getElementById("showPictures").innerHTML = innerHtml;
+}
+function setSizesShowPictures() {
+    var showPictureElements = document.getElementsByClassName("show");
+    for (var i = 0; i < showPictureElements.length; i++){
+        showPictureElements[i].style.width = (screen.width/showPictureElements.length) + "px";
+        showPictureElements[i].style.height = (screen.width/showPictureElements.length) * (2/3) + "px";
+    }
+}
