ec46f744c7a3118b5fb21654548d7622c50c6046
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2018-09-27 15:20:16 +0200

    refactor

--- main.js
+++ main.js
@@ -1,43 +1,23 @@
 
 window.onload = function(){
-    setShowPictures();
-    setDisplayPictures();
+    setPictures();
 };
 
-
-function setShowPictures() {
-    setPicturesInHtml();
-    setSizesShowPictures();
+function setPictures() {
+    setPicturesInHtml("showPictures", 12, "show");
+    setPicturesInHtml("displayPictures", 3, "display");
+    setSizesPicturesOfClass("show");
+    setSizesPicturesOfClass("display");
 }
-function setPicturesInHtml() {
+function setPicturesInHtml(divID, amount, className) {
     var innerHtml = "";
-    for (var i = 1; i < 13; i++){
-        innerHtml += '<img src="pictures/' + i + '.png" class="show">';
+    for (var i = 1; i < amount + 1; i++){
+        innerHtml += '<img src="pictures/' + i + '.png" class="' + className + '">';
     }
-    document.getElementById("showPictures").innerHTML = innerHtml;
+    document.getElementById(divID).innerHTML = innerHtml;
 }
-function setSizesShowPictures() {
-    var showPictureElements = document.getElementsByClassName("show");
-    for (var i = 0; i < showPictureElements.length; i++){
-        showPictureElements[i].style.width = (screen.width/showPictureElements.length) + "px";
-        showPictureElements[i].style.height = (screen.width/showPictureElements.length) * (2/3) + "px";
-    }
-}
-
-
-function setDisplayPictures() {
-    setDisplayPicturesInHtml();
-    setSizesDisplayPictures();
-}
-function setDisplayPicturesInHtml() {
-    var innerHtml = "";
-    for (var i = 1; i < 4; i++){
-        innerHtml += '<img src="pictures/' + i + '.png" class="displayPicture">';
-    }
-    document.getElementById("displayPictures").innerHTML = innerHtml;
-}
-function setSizesDisplayPictures() {
-    var showPictureElements = document.getElementsByClassName("displayPicture");
+function setSizesPicturesOfClass(className) {
+    var showPictureElements = document.getElementsByClassName(className);
     for (var i = 0; i < showPictureElements.length; i++){
         showPictureElements[i].style.width = (screen.width/showPictureElements.length) + "px";
         showPictureElements[i].style.height = (screen.width/showPictureElements.length) * (2/3) + "px";
